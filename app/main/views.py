360b0edd40801d27763d0dc7f539324ae0c663f6
chenglp <chenglongping@100tal.com>
2016-10-10 11:35:22 +0800

    add requirements.txt and views.py

--- app/main/views.py
+++ app/main/views.py
@@ -4,3 +4,8 @@ from . import main
 @main.route(r'/hello/', methods=['GET', 'POST'])
 def hello():
     return "<p>hello</p>"
+
+
+@main.route(r'/', methods=['GET',])
+def index():
+    return "<p>index</p>"
