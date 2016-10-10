8c4f1171656e775a2445f4e86c4f165ea066afc0
chenglp <chenglongping@100tal.com>
2016-10-10 14:09:25 +0800

    提交views修改

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,15 @@
+from flask import url_for
+
 from . import main
 
 
 @main.route(r'/hello/', methods=['GET', 'POST'])
 def hello():
-    return "<p>hello</p>"
+    return "<p>hello</p><a href=%s>index</a>" % url_for('main.index')
 
 
 @main.route(r'/', methods=['GET',])
 def index():
-    return "<p>index</p>"
+    return "<p>index</p><a href=%s>hello</a>" % url_for('main.hello')
+
+
